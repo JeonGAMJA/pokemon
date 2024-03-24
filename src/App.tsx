bb7cf70f07d3da1afb53ec967d71f57784e7dae6
GamjaJEON <indian_potato@naver.com>
2024-03-25 03:16:07 +0900

    feat: pokeApi 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useGetPokemonByNameQuery } from './services/pokemon';
+
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  // 자동으로 데이터를 패치하고 쿼리 값을 가져오는 쿼리 hook을 사용
+  const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur');
+  // 각각의 hooks은 생성된 엔드포인트에서도 접근 가능함
+  // const { data, error, isLoading } = pokemonApi.endpoints.getPokemonByName.useQuery('bulbasaur')
+  console.log(data);
+  return (
+    <div>
+      {error ? (
+        <>Oh no,there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>{data.species.name}</h3>
+          <img src={data.sprites.front_shiny} alt={data.species.name} />
+        </>
+      ) : null}
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+import App from './App.tsx';
+import { store } from './redux/store.ts';
+
+import './index.css';
+
+const rootElement = document.getElementById('root');
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>,
+  rootElement,
+);
